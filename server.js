d4d2f8adc69994536f94a92a9c6ac6d0d694b016
crealu <cameron.miller294@gmail.com>
2024-05-15 15:48:55 -0700

    Update server.js
    Implmented sheets update

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-// const { google } = require('googleapis');
+const { google } = require('googleapis');
 
 require('dotenv').config({ path: '.env' }); 
 
 const app = express();
 const port = process.env.PORT;
 
-// const CLIENT_ID = process.env.CLIENT_ID;
-// const CLIENT_SECRET = process.env.CLIENT_SECRET;
-// const REDIRECT_URI = '/';
-// const REFRESH_TOKEN = 'YOUR_REFRESH_TOKEN';
+const credentials = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+};
 
-// const auth = new google.auth.OAuth2(
-//   CLIENT_ID,
-//   CLIENT_SECRET,
-//   REDIRECT_URI
-// );
-// const sheets = google.sheets({ version: 'v4', auth });
+const auth = new google.auth.GoogleAuth({
+  credentials,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+const sheets = google.sheets({ version: 'v4', auth });
+const sheetId = process.env.GOOGLE_SHEET_ID
 
 app.use(express.static(__dirname));
 app.use(express.urlencoded({extended: true}));
@@ -29,53 +38,47 @@ app.get('/', (req, res) => {
 	res.sendFile('index.html', {root: './'});
 });
 
-app.post('/new-member', (req, res) => {
-	console.log(req.body);
-	const output = `
-		<h3>New Membership Request</h3>
-		<p>Email: ${req.body.email}<br/></p>
-	`;
+app.post('/new-member', async (req, res) => {
+	const { email } = req.body;
 
-	let transporter = nodemailer.createTransport({
-		service: 'Gmail',
-		auth: {
-			user: process.env.THE_EMAIL,
-			pass: process.env.THE_KEY
-		}
-	});
+	try {
+		const response = await sheets.spreadsheets.values.append({
+      spreadsheetId: sheetId,
+      range: 'Sheet1!A1',
+      valueInputOption: 'RAW',
+      requestBody: { values: [[email]] },
+    });
 
-	let mailOptions = {
-    from: 'Niheigo Support <niheigodev@site.com>',
-    to: 'niheigodev@gmail.com',
-		subject: 'Membership Request',
-		text: 'text',
-		html: output
-	};
+  	const output = `
+			<h3>New Membership Request</h3>
+			<p>Email: ${email}<br/></p>
+		`;
 
-	transporter.sendMail(mailOptions, (err, info) => {
-		if (err) { return console.log(err) }
-		console.log(info);
-	});
+		let transporter = nodemailer.createTransport({
+			service: 'Gmail',
+			auth: {
+				user: process.env.THE_EMAIL,
+				pass: process.env.THE_KEY
+			}
+		});
 
-	res.send({msg: 'success'});
+		let mailOptions = {
+	    from: 'Niheigo Support <niheigodev@site.com>',
+	    to: 'niheigodev@gmail.com',
+			subject: 'Membership Request',
+			text: 'text',
+			html: output
+		};
+
+		transporter.sendMail(mailOptions, (err, info) => {
+			if (err) { return console.log(err) }
+			console.log(info);
+		});
+    res.status(200).send({msg: 'Data submitted successfully'});
+	} catch (err) {
+		console.error(err);
+		res.status(200).send({msg: 'Data submitted successfully'});
+	}
 });
-
-// app.post('/new-member-row', async (req, res) => {
-// 	try {
-// 		const { email } = req.body;
-// 		const response = await sheets.spreadsheets.values.append({
-//       spreadsheetId: '1fbb04xkI0LN4t6ELU3LzR4A43UNj92ErmlXJ8oaAEkM',
-//       range: 'Sheet1!A',
-//       valueInputOption: 'RAW',
-//       requestBody: {
-//         values: [[email]],
-//       },
-//     });
-// 	} catch (err) {
-// 		console.log(err);
-// 	}
-// })
-
-
 
 app.listen(port, () => console.log('listening on 4400'));
